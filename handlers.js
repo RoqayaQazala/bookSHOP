8b722bd6c564190174615aa4bfdc4922da69b065
Roqaya Qazala <roukamohammad@gmail.com>
2017-09-26 09:50:39 +0300

    server

--- handlers.js
+++ handlers.js
@@ -1,1 +1,43 @@
 
+var Book = require('./models/book.js');
+var User = require('./models/userModel.js');
+var jwt = require('jwt-simple');
+
+
+module.exports.handleUsers = {
+  signin : function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    User.findOne({username: username})
+      .then(function (user) {
+        if (!user) {
+          res.status(404).json("user not found")
+        } else {
+          user.comparePasswords(password)
+            .then(function (isMatch) {
+              if (isMatch) {
+                var token = jwt.encode(user, 'secret');
+                res.json({token : token, user : user});
+              } else {
+                res.json("password not matched")
+              }
+            });
+        }
+      });
+  },
+
+ 
+
+
+module.exports.handelBook = {
+
+	showbook: function(req, res)  {
+		Book.getBooks(function(err, books)  {
+			if(err){
+				throw err;
+			}
+			res.json(books);
+		});
+	},
+}
