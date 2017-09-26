f9c713f522ee13e8684c3c0256965f9b4e3a9416
Roqaya Qazala <roukamohammad@gmail.com>
2017-09-26 11:57:17 +0300

    n

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,14 @@
 
+angular.module('books',
+ [
+ 'book.services'
+	]);
+.config(function(){
+
+	
+    .when('/signin', {
+      templateUrl: `app/account/signin.html`,
+      controller: 'AuthController'
+    })     
+
+})
